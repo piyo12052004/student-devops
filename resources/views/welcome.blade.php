e6997983b763497a1c88444435f02ab141c3cc3d
pyo_awndi <154031276+piyo12052004@users.noreply.github.com>
2026-08-15 17:04:48 +0700

    test ci cd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
                         <div class="ml-4">
 
                             <h2 class="text-xl font-bold text-green-600">
-                                Project Dev 2
+                                Project Dev 3
                             </h2>
                         </div>
 
